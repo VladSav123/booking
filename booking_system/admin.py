d805fc3babc396d6b88306ba9ee1ed19d961e001
VladSav123 <vsavchin39@gmail.com>
2024-02-24 15:10:26 +0200

    12321

---
+++ booking_system/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from booking_system.models import *
+
+# Register your models h]ere.
+admin.site.register(User)
+admin.site.register(Train)
+admin.site.register(Booking)
